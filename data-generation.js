d906ecc5075fc76644ceb27988d419e9eb43373b
Adam Durrant <adamdurrant92@gmail.com>
2023-02-03 15:17:48 +0000

    added headings and tooltips to bar charts using dummy data

--- data-generation.js
+++ data-generation.js
@@ -1,20 +1,22 @@
 //DONUT
 
 //CHARTS
-const createChart = (percentages, containerId) => {
+const createChart = (title, percentages, containerId) => {
   const chartContainer = document.getElementById(containerId);
+  const heading = document.createElement("h4");
   const chart = document.createElement("div");
+  heading.innerHTML = title;
   chart.classList.add("chart");
 
   percentages.forEach((percentage) => {
     const bar = document.createElement("div");
-    bar.classList.add("bar");
+    bar.classList.add("bar", "tooltip");
     bar.setAttribute("data-tooltip", `${percentage}%`);
     bar.style.flexBasis = `${percentage}%`;
     chart.appendChild(bar);
   });
-
   chartContainer.appendChild(chart);
+  chartContainer.prepend(heading);
 };
 
 //Dummy data
@@ -24,7 +26,7 @@ const data3 = [15, 25, 20, 20, 15, 5];
 const data4 = [50, 25, 15, 5, 5, 0];
 
 //Invocations
-createChart(data1, "chartContainer1");
-createChart(data2, "chartContainer2");
-createChart(data3, "chartContainer3");
-createChart(data4, "chartContainer4");
+createChart("Avg. all", data1, "chartContainer1");
+createChart("Cautious", data2, "chartContainer2");
+createChart("Complaint", data3, "chartContainer3");
+createChart("Confident", data4, "chartContainer4");
